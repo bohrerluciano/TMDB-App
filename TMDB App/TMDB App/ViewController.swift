8e251f21bbc88b8c5ea1e0d0d68f450a6026f8da
Luciano Bohrer <luciano_acosta@sicredi.com.br>
2018-09-20 19:46:39 -0300

    Initial project setup

---
+++ TMDB App/TMDB App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMDB App
+//
+//  Created by Luciano Bohrer on 19/09/18.
+//  Copyright © 2018 Luciano Bohrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
